ed0ac6e8579dd6865b68162c50731bb29d94c2e2
JinseokCodeDiary <qkrwlstjr57@gmail.com>
2024-07-05 17:37:06 +0900

    main.dart

--- Flutter_quest/quest02/main.dart
+++ Flutter_quest/quest02/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '/my_home_paga.dart';
+import '/my_home_paga.dart'; // 두개의 dart파일을 사용하기 때문에 import
 
 void main(){
   runApp(const MyApp());
@@ -19,3 +19,4 @@ class MyApp extends StatelessWidget{
       );
     }
   }
+// 팀원 덕분에 두개의 dart파일을 이용해서 깔끔하게 작성하는 것을 알게 되었고, 체계적으로 진행해서 하면서 배웠다.
